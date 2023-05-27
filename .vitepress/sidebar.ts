1b042d1fe6ed7aaec374c61109871bf301548600
ChandlerVer5 <ChandlerVer5@gmail.com>
2023-05-27 11:31:23 +0800

    feat: 🎸 用 index 代替 README

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -1,7 +1,7 @@
 import { getSideBar } from './plugins/autoSidebar'
 
 export default getSideBar('docs', {
-  indexLink: "README",
+  indexLink: "index",
   sortBy: (path) => {
     const res = /(\d+).+?\/(\d+)\.(\d+)?/.exec(path)
     return res ? Number(res[2]) + Number(res[3]) : 0
